6da11468475559439209eeab8594a5a8f52fb527
Alexey Shcheglov <as.shcheglov@gmail.com>
2019-07-17 15:17:53 +0300

    Banking processing service - Eureka client/Feign clients implemented

---
+++ processing-service/src/main/java/ru/shcheglov/cloud/ProcessingRest.java
@@ -0,0 +1,54 @@
+package ru.shcheglov.cloud;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class ProcessingRest {
+
+    @Autowired
+    private ProcessingRepository repo;
+
+    @Autowired
+    private AccountServiceClient accountServiceClient;
+
+    @Autowired
+    private CardServiceClient cardServiceClient;
+
+    @RequestMapping("/issue/{accountId}")
+    public String issueNewCard(@PathVariable Integer accountId) {
+        final String card = cardServiceClient.createCard();
+        ProcessingEntity pe = new ProcessingEntity();
+        pe.setCard(card);
+        pe.setAccountId(accountId);
+        repo.save(pe);
+        return card;
+    }
+
+    @RequestMapping("/checkout/{card}")
+    public boolean checkout(@PathVariable String card, @RequestParam BigDecimal sum) {
+        ProcessingEntity pe = repo.findByCard(card);
+        if (pe == null) {
+            return false;
+        }
+        return accountServiceClient.checkout(pe.getAccountId(), sum);
+    }
+
+    @RequestMapping("/get")
+    public Map<Integer, String> getByAccount(@RequestParam("account_id") List<Integer> accountIdList) {
+        List<ProcessingEntity> list = repo.findByAccountIdIn(accountIdList);
+        Map<Integer, String> map = new HashMap<Integer, String>();
+        for (ProcessingEntity pe: list) {
+            map.put(pe.getAccountId(), pe.getCard());
+        }
+        return map;
+    }
+}
---
+++ processing-service/src/main/java/ru/shcheglov/cloud/ProcessingService.java
@@ -0,0 +1,16 @@
+package ru.shcheglov.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class ProcessingService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessingService.class, args);
+    }
+}
