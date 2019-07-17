3a7e6636c84adda9afe0747a680c466798e886f4
Alexey Shcheglov <as.shcheglov@gmail.com>
2019-07-17 17:54:55 +0300

    Config server implementation added

--- processing-service/src/main/java/ru/shcheglov/cloud/AccountServiceClient.java
+++ processing-service/src/main/java/ru/shcheglov/cloud/AccountServiceClient.java
@@ -1,6 +1,7 @@
 package ru.shcheglov.cloud;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
--- processing-service/src/main/java/ru/shcheglov/cloud/CardServiceClient.java
+++ processing-service/src/main/java/ru/shcheglov/cloud/CardServiceClient.java
@@ -1,6 +1,7 @@
 package ru.shcheglov.cloud;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient(name = "CardService", fallback = CardServiceFallback.class)
