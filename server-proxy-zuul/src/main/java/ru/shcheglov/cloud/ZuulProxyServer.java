83720c3794f491fdd1adde6bc3efca43e17407b8
Alexey Shcheglov <as.shcheglov@gmail.com>
2019-07-17 17:53:34 +0300

    Zuul proxy server added

---
+++ server-proxy-zuul/src/main/java/ru/shcheglov/cloud/ZuulProxyServer.java
@@ -0,0 +1,17 @@
+package ru.shcheglov.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class ZuulProxyServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulProxyServer.class, args);
+    }
+}
+
