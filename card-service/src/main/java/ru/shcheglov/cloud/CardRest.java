e421dcec4e3aae44a7ffc14d94e53a773c1939f9
Alexey Shcheglov <as.shcheglov@gmail.com>
2019-07-17 15:17:10 +0300

    Banking card service - Eureka client implemented

---
+++ card-service/src/main/java/ru/shcheglov/cloud/CardRest.java
@@ -0,0 +1,17 @@
+package ru.shcheglov.cloud;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CardRest {
+
+    @Autowired
+    private CardNumberGenerator generator;
+
+    @RequestMapping("create")
+    public String createNewCard() {
+        return generator.generate();
+    }
+}
