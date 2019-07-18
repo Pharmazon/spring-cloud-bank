efd33c94133036a1ea28358f8f6737483a766836
Alexey Shcheglov <as.shcheglov@gmail.com>
2019-07-18 20:10:35 +0300

    Minor bugs fixed

--- server-zipkin/src/main/java/ru/shcheglov/cloud/ZipkinServer.java
+++ server-zipkin/src/main/java/ru/shcheglov/cloud/ZipkinServer.java
@@ -1,13 +1,21 @@
 package ru.shcheglov.cloud;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import zipkin2.server.internal.EnableZipkinServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableZipkinStreamServer
+@EnableZipkinServer
 public class ZipkinServer {
 
     public static void main(String[] args) {
         SpringApplication.run(ZipkinServer.class, args);
     }
+
+//    public static void main(String[] args) {
+//        new SpringApplicationBuilder(ZipkinServer.class)
+//                .listeners(new RegisterZipkinHealthIndicators())
+//                .properties("spring.config.name=zipkin-server").run(args);
+//    }
 }
