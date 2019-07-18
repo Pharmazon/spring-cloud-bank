65b3a9ce70b78a7db911189df117a5adc32cac2b
Alexey Shcheglov <as.shcheglov@gmail.com>
2019-07-18 12:29:43 +0300

    Turbine implementation

--- hystrix-dashboard/src/main/java/ru/shcheglov/cloud/HystrixDashboard.java
+++ hystrix-dashboard/src/main/java/ru/shcheglov/cloud/HystrixDashboard.java
@@ -3,11 +3,13 @@ package ru.shcheglov.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
  * GET http://localhost:8900/hystrix
  */
 
+@EnableTurbine
 @EnableHystrixDashboard
 @SpringBootApplication
 public class HystrixDashboard {
