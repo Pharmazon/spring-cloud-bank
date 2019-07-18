449b898f00f85b7b8241b53d83f450e0801d2aad
Alexey Shcheglov <as.shcheglov@gmail.com>
2019-07-18 13:30:58 +0300

    Turbine server implemented

---
+++ server-turbine/src/main/java/ru/shcheglov/cloud/TurbineServer.java
@@ -0,0 +1,14 @@
+package ru.shcheglov.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+
+@SpringBootApplication
+@EnableTurbineStream
+public class TurbineServer {
+    public static void main(String[] args) {
+        SpringApplication.run(TurbineServer.class, args);
+    }
+}
+
